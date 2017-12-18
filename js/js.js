548548e481855006fecab9f8e797ee72956a59cf
ximenes <ximenes130@gmail.com>
2017-12-18 17:35:40 -0200

    Adicionando carregamento de secções via Ajax

--- js/js.js
+++ js/js.js
@@ -1,1 +1,16 @@
-$('.header .content').css('height', rainbowlines.height);
+$(document).ready(function(){
+	$('.header .content').css('height', rainbowlines.height);
+	
+	$(document).on('click', '.tab', (function(e){
+		var url = $(this).find('a').attr('href');
+		e.preventDefault();
+		$.ajax({
+			type: "GET",
+			url: url,
+			data: { },
+			success: function(data){
+				$('#targetElement').html(data);
+			}
+		});
+	}));
+});
