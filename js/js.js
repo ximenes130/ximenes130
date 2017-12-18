8042312ca4eebe6afacba366fc6f5ec4dc7d6424
ximenes <ximenes130@gmail.com>
2017-12-18 18:00:40 -0200

    Exibindo

--- js/js.js
+++ js/js.js
@@ -10,6 +10,11 @@ $(document).ready(function(){
 			data: { },
 			success: function(data){
 				$('#page-content').html(data);
+				$('.progress').css('display', 'none');
+			},
+			error: function(){
+				$('#page-content').html('<h5>Error 404: Página não encontrada</h5>');
+				$('.progress').css('display', 'none');
 			}
 		});
 	}));
