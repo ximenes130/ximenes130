deb427de888e8fbbc7b7144ef4f0962c5fbffb60
ximenes <ximenes130@gmail.com>
2017-12-20 10:24:01 -0200

    Possibilitando que cada conteudo de pagina seja carregado a partir da url

--- js/js.js
+++ js/js.js
@@ -1,22 +1,32 @@
 $(document).ready(function(){
 	$('.header .content').css('height', rainbowlines.height);
+	console.log(document.location.hash);
+
+	if(document.location.hash != '')
+		loadPCUrl(jQuery.Event("click"), document.location.hash);
 	
-	$(document).on('click', '.tab', (function(e){
-		$('.progress').css('display', 'block');
-		var url = $(this).find('a').attr('href');
-		e.preventDefault();
-		$.ajax({
-			type: "GET",
-			url: url,
-			data: { },
-			success: function(data){
-				$('#page-content').html(data);
-				$('.progress').css('display', 'none');
-			},
-			error: function(){
-				$('#page-content').html('<h5>Error 404: Página não encontrada</h5>');
-				$('.progress').css('display', 'none');
-			}
-		});
-	}));
+	$(document).on('click', '.tab', loadPC);
 });
+
+var loadPC = function(e){
+	loadPCUrl(e,$(this).find('a').attr('href'));
+};
+var loadPCUrl = function(e, url){
+	$('.progress').css('display', 'block');
+	e.preventDefault();
+	$.ajax({
+		type: "GET",
+		url: url.slice(1) + '.html',
+		data: { },
+		success: function(data){
+			$('#page-content').html(data);
+			document.location.hash = url;
+			$('.progress').css('display', 'none');
+		},
+		error: function(){
+			$('#page-content').html('<h5>Error 404: Página não encontrada</h5>');
+			document.location.hash = '';
+			$('.progress').css('display', 'none');
+		}
+	});
+};
